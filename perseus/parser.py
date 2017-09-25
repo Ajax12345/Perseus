71d4c4ebca4900b26aa5ae369c3c09f7ddaf356a
Ajax1234 <pentester18@gmail.com>
2017-09-25 15:06:44 -0400

    Added "Access" and "Auto" operations

--- perseus/parser.py
+++ perseus/parser.py
@@ -32,13 +32,192 @@ class InvalidOutputStatement(Exception):
     def __init__(self, message):
         Exception.__init__(self, message)
 
+class IndexOutOfBoundsError(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+errors = []
+
+class ListData:
+    #TODO: modify assignment statements to be able to store function returns, list indices, etc.
+    __slots__ = ["line", "final_list", "seen_assignment", "forbidden", "line_number", "seen_first_bracket", "variable_name", "current_variable", "index", "seen_last_bracket"]
+    def __init__(self, line, line_number, **args):
+        self.line = line
+        self.final_list = []
+        self.seen_assignment = False
+        self.line_number = line_number
+        self.seen_first_bracket = False
+        self.forbidden = ["+", "-", "/", "*", "%"]
+        self.variable_name = ''
+        self.flag = args.get("flag", False)
+        self.current_variable = ''
+        self.new_final_line = []
+        if args.get("flag", False):
+            self.new_final_line = self.initialize_list(self.line)
+
+        self.index = ''
+        self.seen_last_bracket = False
+        if args.get("flag", False):
+            print "new_final_line", self.new_final_line
+
+
+    def initialize_list(self, line):
+        print "line", line
+        if line[0] == ";":
+            return self.final_list
+
+        else:
+            if not self.seen_assignment:
+                if line[0] == " ":
+                    if line[1].isdigit() or line[1].isalpha():
+                        raise InvalidSyntaxError("Line {}: cannot have spaces in variable assignment".format(self.line_number))
+
+                    else:
+                        return self.initialize_list(line[1:])
+
+                elif line[0] in self.forbidden:
+                    raise InvalidSyntaxError("Line {}: forbidden character '{}' in List object assignment".format(self.line_number, line[0]))
+
+
+                elif line[0] == "=":
+                    self.seen_assignment = True
+                    return self.initialize_list(line[1:])
+
+                elif line[0].isdigit() or line[0].isalpha():
+                    self.variable_name += line[0]
+                    return self.initialize_list(line[1:])
+
+            else:
+                if line[0] == " ":
+                    return self.initialize_list(line[1:])
+
+                elif line[0] == "[":
+                    self.seen_first_bracket = True
+                    return self.initialize_list(line[1:])
+                elif line[0] == "{":
+                    raise InvalidSyntaxError("Line {}: invalid list declaration".format(self.line_number))
+
+                elif line[0] == "]":
+                    if not self.seen_first_bracket:
+                        raise InvalidSyntaxError("Line {}: must have corresponding ']' bracket".format(self.line_number))
+
+                    else:
+                        if line[1] != ";":
+                            raise InvalidSyntaxError("Line {}:must include closing ';'".format(self.line_number))
+                        self.final_list.append(self.current_variable)
+                        return self.initialize_list(line[1:])
+
+                elif line[0].isdigit() or line[0].isalpha() or line[0] == ".":
+                    self.current_variable += line[0]
+                    return self.initialize_list(line[1:])
+                elif line[0] == '"':
+                    self.current_variable += line[0]
+                    return self.initialize_list(line[1:])
+
+                elif line[0] == ",":
+                    self.final_list.append(self.current_variable)
+
+                    self.current_variable = ''
+                    return self.initialize_list(line[1:])
+
+    def __iter__(self):
+        if self.flag:
+            return self.new_final_line.__iter__()
+
+
+class AssignValue(ListData):
+    __slots__ = ["final_data"]
+    def __init__(self, line, line_number):
+        ListData.__init__(self, line, line_number, flag = False)
+
+        self.final_data = self.get_assigment_data(line)
+
+    def get_assigment_data(self, line):
+
+        if line[0] == ";":
+            return [self.variable_name, self.current_variable, self.index]
+        else:
+            if not self.seen_assignment:
+                if not self.seen_first_bracket:
+                    if line[0].isalpha() or line[0].isdigit():
+                        self.variable_name += line[0]
+                        return self.get_assigment_data(line[1:])
+
+                    elif line[0] == "[":
+                        self.seen_first_bracket = True
+                        return self.get_assigment_data(line[1:])
+
+                    else:
+                        raise InvalidSyntaxError("Line {}: unauthorized character".format(self.line_number))
+
+                elif self.seen_first_bracket and not self.seen_last_bracket:
+                    if line[0].isdigit():
+                        self.index += line[0]
+                        return self.get_assigment_data(line[1:])
+
+                    elif line[0] == "]":
+                        self.seen_last_bracket = True
+                        return self.get_assigment_data(line[1:])
+
+                    else:
+                        raise InvalidSyntaxError("Line {}: index can only contain integer values".format(self.line_number))
+
+                elif self.seen_last_bracket and self.seen_first_bracket:
+                    if line[0] == " ":
+                        return self.get_assigment_data(line[1:])
+
+                    elif line[0] == ">":
+                        self.seen_assignment = True
+                        return self.get_assigment_data(line[1:])
+
+                    else:
+                        raise InvalidSyntaxError("Line {}:invalid access statement".format(self.line_number))
+
+            else:
+
+                if line[0] == " ":
+                    return self.get_assigment_data(line[1:])
+
+                elif line[0].isdigit() or line[0].isalpha():
+                    self.current_variable += line[0]
+                    return self.get_assigment_data(line[1:])
+
+                else:
+                    raise InvalidSyntaxError("Line {}: invalid access declaration".format(self.line_number))
+
+
+
+
+
+    def variable_value(self):
+        pass
+
+    def __getattr__(self, name):
+        if name == "index":
+            return self.index
+
+        elif name == "variable":
+            return self.current_variable
+
+        elif name == "list":
+            return self.variable_name
+
+
+
+
 class Parser:
     """
-    types: string, int, double, boolean
+    types: string, int, double, boolean, list
     """
     #TODO: find syntax errors: intval2=34; should be int val2 = 456;
     #TODO: add all errors to error queue, raise one error, and list issues
     #IDEA: separate file for Perseus errors?
+    #IDEA: classes for each keyword (token)?
+    #TODO: every method should have a final else statement that throws an error
+    #TODO: create 'increment' and 'decrement' keywords
+    #TODO: modify assignment statements to be able to store function returns, list indices, etc.
+    #TODO: support negative indicing?
+
     __slots__ = ["file", "data_tree", "last_variable", "line_number", "operation_names", "print_queue"]
     def __init__(self, the_file):
         self.file = the_file
@@ -115,6 +294,17 @@ class Parser:
                     raise SyntaxError("Line {}: declared {} of type integer, but found value of not pure string".format(self.line_number, variable))
 
 
+    def auto_parser(self, line, variable):
+
+        if line[0] == ";":
+            return variable
+
+        else:
+            if line[0].isdigit() or line[0].isalpha():
+                variable += line[0]
+                return self.auto_parser(line[1:], variable)
+            else:
+                raise InvalidSyntaxError("Line {}: invalid line declaration".format(self.line_number))
 
 
     def parse_double_declaration(self, line, need_digit, variable, value):
@@ -275,7 +465,7 @@ class Parser:
                 elif line[0] in ["+", "-", "*", "/"]:
 
                     if line[1:]:
-                        if line[1] != " " and line[1].isdigit() and line[1] != '"':
+                        if line[1] != " " and line[1].isdigit() and line[1] != '"': #used to be: if line[1] != " " and line[1].isdigit() and line[1] != '"':
                         #if not line[1].isdigit() or line[1] != '"':
                             raise InvalidSyntaxError("Line {}: unexpected end of declaration".format(self.line_number))
 
@@ -346,19 +536,6 @@ class Parser:
 
             else:
                 raise InvalidSyntaxError("Line {}:output statement cannot contain spaces or operators".format(self.line_number))
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -504,8 +681,63 @@ class Parser:
 
                     self.print_queue.append(self.data_tree[possibilites[0]][variable])
 
+            elif re.findall("^List\s", line[0]):
+                the_list = ListData(line[0][len("List "):], self.line_number, flag=True)
+                variable_name = the_list.variable_name
+                new_list = [i for i in the_list]
+                print "variable_name", variable_name, "the_list", new_list
+
+                new_final_list = []
+                for variable in new_list:
+                    if variable.startswith('"') and not variable.endswith('"'):
+                        raise InvalidSyntaxError("Line {}:unbalanced quotes around string declaration".format(self.line_number))
+
+                    elif not variable.startswith('"') and variable.endswith('"'):
+                        raise InvalidSyntaxError("Line {}:unbalanced quotes around string declaration".format(self.line_number))
+
+                    elif variable.startswith('"') and variable.endswith('"'):
+                        new_final_list.append(variable[1:-1])
+
+                    elif re.search('^\d+$', variable) is not None and re.search('^\d+$', variable).group(0) == variable:
+                        new_final_list.append(int(variable))
+
+                    elif re.search('^\d+\.\d+$', variable) is not None and re.search('^\d+\.\d+$', variable).group(0) == variable:
+                        new_final_list.append(float(variable))
+
+                    elif variable in ["True", "False"]:
+                        new_final_list.append((lambda x:{"True":True, "False":False}[x])(variable))
 
 
+                    else:
+                        if variable.isalnum() and variable not in ["True", "False"]:
+                            possibilites = [i for i in ["Double", "Boolean", "String", "List"] if variable in self.data_tree[i]]
+                            if not possibilites:
+                                raise VariableNotDeclared("Line {}: variable '{}' not declared".format(self.line_number, variable))
+
+                            new_final_list.append(self.data_tree[possibilites[0]][variable])
+
+                self.data_tree["List"][variable_name] = new_final_list
+
+
+            elif re.findall("^access\s", line[0]):
+                access_data = AssignValue(line[0][len("access "):], self.line_number)
+                index = access_data.index
+                list_name = access_data.list
+                variable_name = access_data.variable
+                if not any(variable_name in self.data_tree[i] for i in ["Auto", "Boolean", "String", "Double", "Integer"]):
+                    raise VariableNotDeclared("Line {}: variable '{}' not found".format(self.line_number, variable_name))
+                if list_name not in self.data_tree["List"]:
+                    raise VariableNotDeclared("Line {}: variable '{}' not found".format(self.line_number, list_name))
+                if int(index) >= len(self.data_tree["List"][list_name]):
+                    raise IndexOutOfBoundsError("Line {}: index of {} is greater than length of List {} (size of {})".format(self.line_number, int(index), list_name, len(self.data_tree["List"][list_name])))
+
+
+            elif re.findall("^auto\s", line[0]):
+                if line[0][-1] != ";":
+                    raise InvalidLineTermination("Line {}: must complete statement with ';'".format(self.line_number))
+
+                new_variable_name = self.auto_parser(line[0][len("auto "):], '')
+                self.data_tree["Auto"][new_variable_name] = None
 
 
 
